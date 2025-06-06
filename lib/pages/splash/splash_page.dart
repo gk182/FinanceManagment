45a0f220587d8b2f6c3e37b8888f24a92cc0288c
gk182 <khaipham182@gmail.com>
2025-06-06 23:49:53 +0700

    update time splash

--- lib/pages/splash/splash_page.dart
+++ lib/pages/splash/splash_page.dart
@@ -16,7 +16,7 @@ class _SplashPageState extends State<SplashPage> {
   }
 
   Future<void> _navigate() async {
-    await Future.delayed(const Duration(seconds: 3));
+    await Future.delayed(const Duration(seconds: 5));
     
     final user = FirebaseAuth.instance.currentUser;
 
